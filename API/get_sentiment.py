b0f699a9c3263676e5ad9798669f2044cc58cc06
Mohit <bluesky5april2021@gmail.com>
2025-08-06 23:57:10 +0530

    Added predict_batches option in get_sentiment API

--- API/get_sentiment.py
+++ API/get_sentiment.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import List
+import numpy as np
+
 from transformers import AutoTokenizer
 import onnxruntime as ort
-import numpy as np
 
 app = FastAPI()
 session = ort.InferenceSession("sequence_classifier.onnx")
@@ -13,10 +15,17 @@ MAX_LEN = 128
 class ReviewRequest(BaseModel):
     review: str
 
+class BatchReviewRequest(BaseModel):
+    reviews: List[str]
+
+# Utitility functions
 def softmax(x):
     e_x = np.exp(x - np.max(x))
     return e_x / e_x.sum()
 
+def softmax_2d(x):
+    e_x = np.exp(x - np.max(x, axis=1, keepdims=True))
+    return e_x / e_x.sum(axis=1, keepdims=True)
 
 @app.get("/")
 def read_root():
@@ -44,5 +53,35 @@ def predict_sentiment(request: ReviewRequest):
             "Positive": float(probs[1])
         }
 
+    except Exception as e:
+        return {"error": str(e)}
+
+
+
+@app.post("/predict_batch")
+def predict_sentiments(request: BatchReviewRequest):
+    try:
+        review_text = request.reviews
+        if not review_text:
+            return {"error": "Reviews List cannot be empty."}
+
+        output = tokenizer(review_text, truncation=True, padding='max_length', max_length=MAX_LEN, return_tensors="pt")
+        inputs = {
+            "input_ids": output["input_ids"].numpy().astype(np.int64),
+            "attention_mask": output["attention_mask"].numpy().astype(np.float32)
+        }
+
+        logits = session.run(None, inputs)[0]
+        probs = softmax_2d(logits)
+
+        predictions = [
+            {
+                "Negative": float(p[0]),
+                "Positive": float(p[1])
+            }
+            for p in probs
+        ]
+
+        return {"predictions": predictions}
     except Exception as e:
         return {"error": str(e)}
